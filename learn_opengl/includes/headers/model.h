dda7106588c5c61a4ca9e8296d8b2a2b6c0c5d47
阿莫 <>
2022-01-12 18:49:59 +0800

    add model

---
+++ learn_opengl/includes/headers/model.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "./shader_m.h"
+#include "./mesh.h"
+
+class Model
+{
+public:
+    /*  函数   */
+    Model(char* path)
+    {
+        loadModel(path);
+    }
+    void Draw(Shader shader);
+private:
+    /*  模型数据  */
+    vector<Mesh> meshes;
+    string directory;
+    /*  函数   */
+    void loadModel(string path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type,
+        string typeName);
+};
---
+++ learn_opengl/src/3_model/3_1_load_model/shader.frag
@@ -0,0 +1,10 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TextCoords;
+uniform sampler2D texture_diffuse;
+
+void main()
+{
+    FragColor = texture(texture_diffuse, TextCoords);
+}
